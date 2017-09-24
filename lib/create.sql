3e6df91953c7c4463ef7c3d43f7197efa6c480bb
github username <github email address>
2017-09-24 12:54:19 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    GENDER CHAR(1),
+    COLOR TEXT,
+    TEMPERAMENT TEXT,
+    ALIVE INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 10, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 12, "F", "Dark Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 10, "M", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 7,"F", "Dark Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 8, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 3, "F", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 4, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 2, "M", "Brown", "Friendly", 1);
